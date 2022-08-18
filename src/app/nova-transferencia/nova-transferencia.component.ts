653c766143f87c1d9a31bf869f4c1c899a753d4b
Antonio Dias <antoniovictor200@gmail.com>
2022-08-18 13:33:02 -0300

    aprendendo a usar variaveis do dom no componente t do angular

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.html'],
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent {}
+export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
+  public transferir() {
+    console.log('transferencia solicitada');
+    console.log(this.valor);
+    console.log(this.destino);
+  }
+}
